8023c9c07749e44c28befc18e8ec0fc3cb0a5bfe
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-04-20 06:48:21 +0500

    Update craker.py

--- craker.py
+++ craker.py
@@ -22,7 +22,7 @@ br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U
 #-Keluar-#
 def keluar():
 	 print "\033[1;91m[!] Exit"
-	os.sys.exit()
+		os.sys.exit()
 	
 #-Warna-#
 def acak(x):
